f8094513ce97d8148afbae2bf66aab643e0c6afd
Eduard <paulescuedward@gmail.com>
2025-03-09 20:09:46 +0200

    Update la gitignore, adaugarea si configurarea bundle

--- NextGenPC/Global.asax.cs
+++ NextGenPC/Global.asax.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using NextGenPC.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
@@ -14,8 +16,9 @@ namespace NextGenPC
         void Application_Start(object sender, EventArgs e)
         {
             // Code that runs on application startup
-           AreaRegistration.RegisterAllAreas();
-           RouteConfig.RegisterRoutes(RouteTable.Routes);
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
