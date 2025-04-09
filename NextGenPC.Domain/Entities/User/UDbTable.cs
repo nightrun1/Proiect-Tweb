4131e7a01081e3416b2296c3f6f19b1699f6eb52
Edward <paulescuedward@gmail.com>
2025-04-09 23:16:21 +0300

    Add package Entity Framework, add folders for DBModel, and add UDbTable class

---
+++ NextGenPC.Domain/Entities/User/UDbTable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NextGenPC.Domain.Entities.User
+{
+    class UDbTable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; } 
+    }
+}
