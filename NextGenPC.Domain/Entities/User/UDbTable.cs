e7c2084bf3905acc5ebbc723c9696b8fa2f2c803
Denis <tihonencod@gmail.com>
2025-04-10 17:36:13 +0300

    adaugat user database table

--- NextGenPC.Domain/Entities/User/UDbTable.cs
+++ NextGenPC.Domain/Entities/User/UDbTable.cs
@@ -12,6 +12,32 @@ namespace NextGenPC.Domain.Entities.User
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int Id { get; set; } 
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Username")]
+        [StringLength(30, MinimumLength = 5, ErrorMessage = "Username cannot be longer than 30 characters and less 5 charachers")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Password")]
+        [StringLength(50, MinimumLength = 6, ErrorMessage = "Password cannot be longer than 50 characters and less 6 charachers")]
+        public string Password { get; set; }
+
+
+        [Required]
+        [Display(Name = "Email")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime LastLogin { get; set; }
+
+        [StringLength(30)]
+        public string LasIp { get; set; }
+
+
+        //need implementation
+        //public URole Level { get; set; }
     }
 }
