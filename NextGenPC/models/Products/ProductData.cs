3b5004eab6a5a5e088d74c9ae0f0f44df20294a4
petee <cojocaru.petru12@gmail.com>
2025-05-26 23:12:43 +0300

    Added products view model

---
+++ NextGenPC/models/Products/ProductData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NextGenPC.Models.Products
+{
+    public class ProductData
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public string Specifications { get; set; }
+        public decimal Price { get; set; }
+    }
+}
