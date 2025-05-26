c1c18142f8a4239b7ac0a036a87afc4202c93f4b
nightrun1 <tihonencod@gmail.com>
2025-05-27 00:13:42 +0300

    Update BusinessLogic filename

---
+++ NextGenPC.BusinessLogic/BusinessLogic.cs
@@ -0,0 +1,18 @@
+﻿using NextGenPC.BusinessLogic.BLStruct;
+using NextGenPC.BusinessLogic.Interfaces;
+
+namespace NextGenPC.BusinessLogic
+{
+    public class BusinesLogic
+    {
+        public ISession GetSessionBL()
+        {
+            return new SessionBL();
+        }
+
+        public IRegister GetRegisterBL()
+        {
+            return new RegisterBL();
+        }
+    }
+}
