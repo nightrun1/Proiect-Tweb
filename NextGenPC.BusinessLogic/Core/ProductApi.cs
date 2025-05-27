94c818adf645a054dfb7702539fa3db73ba1f3b5
petee <cojocaru.petru12@gmail.com>
2025-05-27 17:43:53 +0300

    Fixed product api

--- NextGenPC.BusinessLogic/Core/ProductApi.cs
+++ NextGenPC.BusinessLogic/Core/ProductApi.cs
@@ -3,7 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
+using NextGenPC.BusinessLogic.DBModel;
 using NextGenPC.Domain.Entities.Product;
+using NextGenPC.Domain.Entities.User;
+using NextGenPC.Domain.Enums;
+using NextGenPC.Helpers.RegFlow;
 
 namespace NextGenPC.BusinessLogic.Core
 {
@@ -11,42 +16,32 @@ namespace NextGenPC.BusinessLogic.Core
     {
         public List<ProdData> GetProducts()
         {
-            return new List<ProdData>
+            using (var db = new ProductContext())
             {
-                new ProdData
+                // Verificam daca exista produse in baza de date
+                if (db.Products.Any())
                 {
-                    Name = "Calculator Gaming Unity X01",
-                    ImageUrl = "https://xstore.md/images/product/thumbs/2025/04/calculator-gaming-unity-x01-xstore-md-49.webp",
-                    CPU = "Ryzen 5 5500",
-                    GPU = "RTX 3050",
-                    RAM = "16GB RAM",
-                    Storage = "512GB SSD",
-                    StockQuantity = 10,
-                    Price = 11199m
-                },
-                new ProdData
-                {
-                    Name = "Setup PC Gaming X01",
-                    ImageUrl = "https://xstore.md/images/product/thumbs/2024/05/setup-240501110029-xstore.webp",
-                    CPU = "Ryzen 5 5500",
-                    GPU = "RTX4060",
-                    RAM = "16GB DDR4",
-                    Storage = "512GB SSD",
-                    StockQuantity = 5,
-                    Price = 23899m
-                },
-                new ProdData
-                {
-                    Name = "Calculator Gaming Raptor X07",
-                    ImageUrl = "https://xstore.md/images/product/thumbs/2025/05/calculator-gaming-raptor-x07-xstore-md-57.webp",
-                    CPU = "i5-12400F",
-                    GPU = "RTX4060",
-                    RAM = "16GB DDR4",
-                    Storage = "1TB SSD",
-                    StockQuantity = 8,
-                    Price = 16799m
+                    // Daca exista, le returnam
+                    return db.Products.Select(p => new ProdData
+                    {
+                        Id = p.Id,
+                        Name = p.Name,
+                        ImageUrl = p.ImageUrl,
+                        CPU = p.CPU,
+                        GPU = p.GPU,
+                        RAM = p.RAM,
+                        Storage = p.Storage,
+                        StockQuantity = p.StockQuantity,
+                        Price = p.Price
+                    }).ToList();
                 }
-            };
+
+                else
+                {
+                    // Daca nu exista, returnam o lista goala
+                    return new List<ProdData>();
+                }
+            }
         }
     }
 }
