67122ca99d18b7212760b30fc7d642043d247650
petee <cojocaru.petru12@gmail.com>
2025-05-27 17:43:09 +0300

    Added product context

---
+++ NextGenPC.BusinessLogic/DBModel/ProductContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NextGenPC.Domain.Entities.Product;
+
+namespace NextGenPC.BusinessLogic.DBModel
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext() :
+            base("name=ShopDB")
+        {
+        }
+
+        public virtual DbSet<ProdDbTable> Products { get; set; }
+    }
+}
