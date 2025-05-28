15c03ea2d56f5d00fa2bdf1010d3f6b2e81d3918
Edward <paulescuedward@gmail.com>
2025-05-28 20:22:59 +0300

    Change Home page, change HomeController

--- NextGenPC/Controllers/HomeController.cs
+++ NextGenPC/Controllers/HomeController.cs
@@ -3,15 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using NextGenPC.BusinessLogic.Interfaces;
+using NextGenPC.LogicHelper;
 
 namespace NextGenPC.Controllers
 {
     public class HomeController : BaseController
     {
+        private readonly IProduct _product;
+        public HomeController()
+        {
+            var bl = new BusinessLogic.BusinessLogic();
+            _product = bl.GetProductBL();
+        }
         // GET: Home
         public ActionResult Index()
         {
             SessionStatus();
+            var latestProducts = _product.GetAllProductsLogic()
+                                 .OrderByDescending(p => p.Id)
+                                 .Take(6)
+                                 .Select(ProductMapper.ToViewModel)
+                                 .ToList();
+
+            ViewBag.NewProducts = latestProducts;
             return View();
         }
     }
